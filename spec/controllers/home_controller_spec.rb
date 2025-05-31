2c81d1302c0d446b1fc29978f6952abf6922925b
Bill Jones <williamjones120862@gmail.com>
2025-05-31 06:45:34 -0600

    Created homepage and user model

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+require 'spec_helper'
+
+RSpec.describe 'HomeController', type: :request do
+  describe "landing" do
+    it "is successful" do
+      get home_landing_url
+      expect(response).to be_successful
+    end
+  end
+end
